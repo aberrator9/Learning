1903a2932e2e4af4fd9a10ca16f0c43128f13f11
Justin Linton <127802772+aberrator9@users.noreply.github.com>
2023-05-20 20:21:05 -0400

    Add javascript text scaling example.

--- Projects/Website/scripts/main.js
+++ Projects/Website/scripts/main.js
@@ -1,26 +1,28 @@
-const myHeading = document.querySelector("h1")
-myHeading.textContent = "PRESS START";
+const myHeading = document.getElementById("functionTest")
 
-// Set the initial scale values
+
+
 let scaleX = 1;
 let scaleY = 1;
 let angle = 0;
 
-// Start the animation
-function animate() {
-    // Calculate the scale values using a sine function
-    scaleX = 1 + Math.sin(angle) * 0.1; // Adjust the amplitude as needed
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+
+function pressStartAnimation() {
+    scaleX = 1 + Math.sin(angle) * 0.08; // Adjust the amplitude as needed
     scaleY = 1 - Math.cos(angle) * 0.03;
 
-    // Apply the scale values using CSS transform
     myHeading.style.transform = `scale(${scaleX}, ${scaleY})`;
 
-    // Increase the angle for the next frame
     angle += 0.07; // Adjust the speed as needed
 
     // Call animate() on the next frame
-    requestAnimationFrame(animate);
+    requestAnimationFrame(pressStartAnimation);
+    myHeading.textContent = "scaleX * scaleY " + multiply(scaleX, scaleY);
 }
 
 // Start the animation loop
-animate();
+pressStartAnimation();
