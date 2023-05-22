62d35ac151307d6230f02760e5dcdbcc80a81a5f
Justin Linton <127802772+aberrator9@users.noreply.github.com>
2023-05-22 09:36:44 -0400

    Add transform button, user name customization.

--- Projects/Website/scripts/main.js
+++ Projects/Website/scripts/main.js
@@ -1,7 +1,32 @@
-const myHeading = document.getElementById("functionTest")
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+let pikaButton = document.getElementById("pikaButton");
+let pikaImg = document.getElementById("pikaImg");
+let pikaImgScaleX = 1;
 
+myButton.onclick = () => {
+    setUserName();
+}
 
+pikaButton.onclick = () => {
+    pikaImgScaleX = -pikaImgScaleX;
+    pikaImg.style.transform = `scaleX(${-pikaImgScaleX})`;
+}
 
+if (!localStorage.getItem("name")) {
+    setUserName();
+} else {
+    const storedName = localStorage.getItem("name");
+    myHeading.textContent = `Finally, an employee who won't ruin your business, ${storedName}`;
+}
+
+function setUserName(name) {
+    const myName = prompt("Please enter your name.")
+    localStorage.setItem("name", myName);
+    myHeading.textContent = `Finally, an employee who won't ruin your business, ${myName}`;
+}
+
+let functionTest = document.getElementById("functionTest")
 let scaleX = 1;
 let scaleY = 1;
 let angle = 0;
@@ -10,18 +35,17 @@ function multiply(num1, num2) {
     return num1 * num2;
 }
 
-
 function pressStartAnimation() {
     scaleX = 1 + Math.sin(angle) * 0.08; // Adjust the amplitude as needed
     scaleY = 1 - Math.cos(angle) * 0.03;
 
-    myHeading.style.transform = `scale(${scaleX}, ${scaleY})`;
+    functionTest.style.transform = `scale(${scaleX}, ${scaleY})`;
 
     angle += 0.07; // Adjust the speed as needed
 
     // Call animate() on the next frame
     requestAnimationFrame(pressStartAnimation);
-    myHeading.textContent = "scaleX * scaleY " + multiply(scaleX, scaleY);
+    functionTest.textContent = "scaleX * scaleY " + multiply(scaleX, scaleY).toFixed(2);
 }
 
 // Start the animation loop
