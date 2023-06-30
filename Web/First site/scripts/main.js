66057e7617e94e595a8d26ac0816a8c38790603d
Justin Linton <127802772+aberrator9@users.noreply.github.com>
2023-06-30 17:30:06 -0400

    CSS eye. Add scroll to top button.

--- Web/First site/scripts/main.js
+++ Web/First site/scripts/main.js
@@ -3,6 +3,7 @@ let myHeading = document.querySelector("h1");
 let pikaButton = document.getElementById("pikaButton");
 let pikaImg = document.getElementById("pikaImg");
 let pikaImgScaleX = 1;
+let toTopButton = document.getElementById("to-top-button");
 
 myButton.onclick = () => {
     setUserName();
@@ -11,6 +12,15 @@ myButton.onclick = () => {
 pikaButton.onclick = () => {
     pikaImgScaleX = -pikaImgScaleX;
     pikaImg.style.transform = `scaleX(${pikaImgScaleX})`;
+}
+
+toTopButton.onclick = () => {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
+window.onscroll = () => {
+    showButton();
 }
 
 if (!localStorage.getItem("name")) {
@@ -26,7 +36,16 @@ function setUserName(name) {
     myHeading.innerHTML = `Finally, an employee who <em>won't</em> ruin your business, ${myName}`;
 }
 
-let functionTest = document.getElementById("functionTest")
+function showButton() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        toTopButton.style.display = "block";
+    } else {
+        toTopButton.style.display = "none";
+    }
+
+}
+
+let squashAndStretchText = document.getElementById("squash-and-stretch")
 let scaleX = 1;
 let scaleY = 1;
 let angle = 0;
@@ -35,17 +54,18 @@ function multiply(num1, num2) {
     return num1 * num2;
 }
 
+
 function squashAndStretch() {
     scaleX = 1 + Math.sin(angle) * 0.08; // Adjust the amplitude as needed
     scaleY = 1 - Math.cos(angle) * 0.03;
 
-    functionTest.style.transform = `scale(${scaleX}, ${scaleY})`;
+    squashAndStretchText.style.transform = `scale(${scaleX}, ${scaleY})`;
 
     angle += 0.07; // Adjust the speed as needed
 
     // Call animate() on the next frame
     requestAnimationFrame(squashAndStretch);
-    functionTest.textContent = "scaleX * scaleY " + multiply(scaleX, scaleY).toFixed(2);
+    squashAndStretchText.textContent = "scaleX * scaleY " + multiply(scaleX, scaleY).toFixed(2);
 }
 
 // Start the animation loop
